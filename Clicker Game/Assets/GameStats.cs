15f07c2753fef7a0e03ee779f2b1045b730c807e
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2023-04-27 17:02:28 -0400

    added upgrade buttona

--- Clicker Game/Assets/GameStats.cs
+++ Clicker Game/Assets/GameStats.cs
@@ -5,16 +5,27 @@ using UnityEngine.UI;
 
 public class GameStats : MonoBehaviour
 {
-    [SerializeField] GameObject woodText;
+    public string resource;
+
+    [SerializeField] Text woodText;
+    [SerializeField] Text stoneText;
+    [SerializeField] Text foodText;
+    // move these to their own button scripts
+    [SerializeField] Button woodUpgradeBtn;
+    [SerializeField] Button stoneUpgradeBtn;
+    [SerializeField] Button foodUpgradeBtn;
 
 
 
-    [SerializeField] int _wood = 0;
-    [SerializeField] int _woodIncrement = 1;
+    private int _wood = 0;
+    private int _woodIncrement = 1;
+    private int _stone = 0;
+    private int _stoneIncrement = 1;
+    private int _food = 0;
+    private int _foodIncrement = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -22,8 +33,23 @@ public class GameStats : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            _wood += _woodIncrement;
-            woodText.GetComponent<Text>().text = "Wood: " + _wood;
+            if (resource.Equals("wood"))
+            {
+                _wood += _woodIncrement;
+                woodText.GetComponent<Text>().text = "Wood: " + _wood;
+            }
+            if (resource.Equals("stone"))
+            {
+                _stone += _stoneIncrement;
+                stoneText.GetComponent<Text>().text = "Stone: " + _stone;
+            }
+            if (resource.Equals("wood"))
+            {
+                _food += _foodIncrement;
+                foodText.GetComponent<Text>().text = "Food: " + _food;
+            }
         }
+
+        if (woodUpgradeBtn.clicked)
     }
 }
